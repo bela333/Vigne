568083480a5a3fe29c779acd8270031873130cc7
bela333 <b3kstudio@gmail.com>
2018-09-30 21:17:28 +0200

    Useful loading messages

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/bela333/Vigne/commands"
 	"github.com/bela333/Vigne/modules/ping"
 	"github.com/bela333/Vigne/server"
@@ -10,17 +11,21 @@ import (
 //TODO: License
 
 func main() {
+	fmt.Print("Creating bot...")
 	s, err := server.NewServer("vigne", "localhost:6379", "")
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(" Done!")
 	//System modules
 	s.RegisterModule(&commands.CommandsModule{})
 	//User modules
 	s.RegisterModule(&ping.PingModule{})
+	fmt.Print("Running bot...")
 	err = s.Start()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(" Done!")
 
 }
--- server/modules.go
+++ server/modules.go
@@ -1,6 +1,9 @@
 package server
 
-import "github.com/bela333/Vigne/errors"
+import (
+	"fmt"
+	"github.com/bela333/Vigne/errors"
+)
 
 type Module interface {
 	GetName() string
@@ -9,8 +12,10 @@ type Module interface {
 
 
 func (s *Server) RegisterModule(m Module)  {
+	fmt.Printf("Loading %s...", m.GetName())
 	m.Init(s)
 	s.Modules[m.GetName()] = m
+	fmt.Println(" Done!")
 }
 
 func (s *Server) GetModuleByName(name string) (Module, error) {
